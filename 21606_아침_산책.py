3de8c059c6b9db224876d8c47fa5cdc4fbe8e0c4
kimhyunso <gustn9515@gmail.com>
2025-07-29 00:53:18 +0900

    feat: 아침산책 문제 풀이

--- 21606_아침_산책.py
+++ 21606_아침_산책.py
@@ -1,42 +1,45 @@
 import sys
-
+sys.setrecursionlimit(10**5)
 sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
 
-N = int(input())
-place = list(map(int, input()))
-place.insert(0, 0)
+n = int(input())
+places = list(input())
+places.insert(0, 0)
+graph = [[] for _ in range(n+1)]
 
-graph = [[] for _ in range(N + 1)]
-visited = [False] * (N + 1)
-count = 0
+visited = [False] * (n+1)
+result = [0] * (n+1)
 
-for _ in range(N - 1):
-    vertex, edge = map(int, input().split())
-    graph[vertex].append(edge)
-    graph[edge].append(vertex)
-    if place[vertex] == 1 and place[edge] == 1:
-        count += 1
-
-def dfs(graph, start, visited):
-    visited[start] = True
-    n = 0
-    for v in graph[start]:
-        if not visited[v]:
-            if place[v] == 1:
-                n += 1
-            elif place[v] == 0:
-                n += dfs(graph, v, visited)
-    return n
+for _ in range(n-1):
+    vetex, edge = map(int, input().split())
+    graph[vetex].append(edge)
+    graph[edge].append(vetex)
     
+def dfs(node, start):
+    if int(places[node]) == 1:
+        result[node] += 1
+        result[start] += 1
+        return 
+    for next_node in graph[node]:
+        if not visited[next_node]:
+            visited[next_node] = True
+            dfs(next_node, start)
+            visited[next_node] = False
 
-for i in range(1, N + 1):
-    if not visited[i] and place[i] == 0:
-        n = dfs(graph, i, visited)
-        count += n * (n - 1) // 2
+ans = 0
+for i in range(1, n+1):
+    if int(places[i]) == 1:
+        visited[i] = True
 
-print(count)
+        for next_node in graph[i]:
+            if int(places[next_node]) == 1:
+                if not visited[next_node]:
+                    result[next_node] += 1
+                    result[i] += 1
+            else:
+                visited[next_node] = True
+                dfs(next_node, i)
+                visited[next_node] = False
 
-
-
-
-
+print(sum(result))
